defb523f2be317bd0d0599c9288f730122af6d0e
thyoshikawa-data <thalesyoshi@gmail.com>
2026-05-04 19:34:19 -0300

    Delete sql/trusted.sql

--- sql/trusted.sql
+++
@@ -1,8 +0,0 @@
--- Camada TRUSTED
-CREATE TABLE TRUSTED_SELIC AS
-SELECT 
-    TO_DATE(data_raw, 'YYYY-MM-DD') AS data,
-    TO_NUMBER(valor_raw) AS valor,
-    data_carga
-FROM RAW_SELIC
-WHERE valor_raw IS NOT NULL;
