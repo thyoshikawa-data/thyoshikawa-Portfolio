9ea609086cedbb34fd8063369be0c8efcb6ec9fc
thyoshikawa-data <thalesyoshi@gmail.com>
2026-05-04 19:21:59 -0300

    Update trusted.sql

--- sql/trusted.sql
+++ sql/trusted.sql
@@ -2,5 +2,7 @@
 CREATE TABLE TRUSTED_SELIC AS
 SELECT 
     TO_DATE(data_raw, 'YYYY-MM-DD') AS data,
-    TO_NUMBER(valor_raw) AS valor
-FROM RAW_SELIC;
+    TO_NUMBER(valor_raw) AS valor,
+    data_carga
+FROM RAW_SELIC
+WHERE valor_raw IS NOT NULL;
