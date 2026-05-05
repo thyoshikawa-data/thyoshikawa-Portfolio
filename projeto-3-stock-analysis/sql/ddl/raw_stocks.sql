a843bd2597ec9cc15d472fc3109802ad1f62375f
thyoshikawa-data <thalesyoshi@gmail.com>
2026-05-04 22:00:15 -0300

    Create raw_stocks.sql

---
+++ projeto-3-stock-analysis/sql/ddl/raw_stocks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE RAW_STOCKS (
+    ticker VARCHAR2(20),
+    data DATE,
+    open NUMBER,
+    high NUMBER,
+    low NUMBER,
+    close NUMBER,
+    volume NUMBER,
+    data_carga TIMESTAMP
+);
